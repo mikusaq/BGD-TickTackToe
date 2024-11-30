7a78552be0a56432121acd7a6c9bdefc2a1d6826
Šťastný Mikuláš (241611) <mikusaq@seznam.cz>
2024-11-30 18:17:12 +0200

    Add GameController, add main game loop

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class GameController : MonoBehaviour
+{
+    public List<TMP_Text> buttonList;
+    private string playerSide;
+
+    void Start()
+    {
+        SetGameControllerReferenceOnButtons();
+        playerSide = "X";
+    }
+
+    void SetGameControllerReferenceOnButtons()
+    {
+        for (int i = 0; i < buttonList.Count; i++)
+        {
+            buttonList[i].GetComponentInParent<GridSpace>().SetGameControllerReference(this);
+        }
+    }
+
+    public string GetPlayerSide()
+    {
+        return playerSide;
+    }
+
+    public void EndTurn()
+    {
+        if ((buttonList[0].text == playerSide && buttonList[1].text == playerSide && buttonList[2].text == playerSide)
+            || (buttonList[3].text == playerSide && buttonList[4].text == playerSide && buttonList[5].text == playerSide)
+            || (buttonList[6].text == playerSide && buttonList[7].text == playerSide && buttonList[8].text == playerSide)
+            || (buttonList[0].text == playerSide && buttonList[3].text == playerSide && buttonList[6].text == playerSide)
+            || (buttonList[1].text == playerSide && buttonList[4].text == playerSide && buttonList[7].text == playerSide)
+            || (buttonList[2].text == playerSide && buttonList[5].text == playerSide && buttonList[8].text == playerSide)
+            || (buttonList[0].text == playerSide && buttonList[4].text == playerSide && buttonList[8].text == playerSide)
+            || (buttonList[2].text == playerSide && buttonList[4].text == playerSide && buttonList[6].text == playerSide))
+        {
+            GameOver();
+        }
+        ChangeSides();
+    }
+
+    void ChangeSides()
+    {
+        if (playerSide == "X")
+        {
+            playerSide = "O";
+        }
+        else
+        {
+            playerSide = "X";
+        }
+    }
+
+    void GameOver()
+    {
+        for (int i = 0; i < buttonList.Count; i++)
+        {
+
+            buttonList[i].GetComponentInParent<Button>().interactable = false;
+        }
+    }
+}
